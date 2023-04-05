9ae7bd7c9cf9d443a58d22da8f1f0b88b37658b8
Khang Hoang <nguyenhoangkhang12102003@gmail.com>
2023-04-06 00:28:39 +0700

    define project and declare all variables

---
+++ src/main/python/bin/get_all_variables.py
@@ -0,0 +1,16 @@
+import os
+# Set All Variables
+os.environ['envn'] = 'TEST'
+os.environ['header'] = 'True'
+os.environ['inferSchema'] = 'True'
+# Get All Variables
+envn = os.environ['envn']
+header = os.environ['header']
+inferSchema = os.environ['inferSchema']
+
+# Set Other Variables
+appName = " USA Prescribers Research Report"
+current_path = os.getcwd()
+staging_dim_city = current_path + '\staging\\dimension_city'
+staging_fact = current_path + '\staging\\fact'
+print(staging_dim_city)
---
+++ src/main/python/bin/run_presc_pipeline.py
@@ -0,0 +1,40 @@
+# Import all the neccessary modules
+import get_all_variables as gav
+
+
+def main():
+    print(gav.header)
+
+    # Get Spark object
+        # Validate Spark Object
+        # Set up logging Configuration Mechanism
+        # Set up Error Handling
+
+
+    # Initiate run_presc_data_ingestion Script
+        # Load the City File
+        # Load the Prescriber Fact File
+        # Validate
+        # Set up logging Configuration Mechanism
+        # Set up Error Handling
+
+    # Initiate run_presc_data_preprocessing Script
+        # Perform data Cleaning Operations
+        # Validate
+        # Set up logging Configuration Mechanism
+        # Set up Error Handling
+
+    # Initiate run_presc_data_transform Script
+        # Apply all the transformation Logics
+        # Validate
+        # Set up logging Configuration Mechanism
+        # Set up Error Handling
+
+    # Initiate run_presc_data_extraction Script
+        # Validate
+        # Set up logging Configuration Mechanism
+        # Set up Error Handling
+
+# End of Applications Part 1
+if __name__ == "__main__":
+    main()
