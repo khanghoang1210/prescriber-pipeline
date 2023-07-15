52181e09c40acc7d2822a0259fa89dd5a204cd19
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2023-07-15 15:19:46 +0700

    configuration logging

--- src/main/python/bin/run_presc_pipeline.py
+++ src/main/python/bin/run_presc_pipeline.py
@@ -3,17 +3,26 @@ import sys
 import get_all_variables as gav
 from create_objects import get_spark_object
 from validations import get_curr_date
+import logging
+import logging.config
+
+# Load the logginf configuration file
+#logging.config.fileConfig(fname='../util/logging_to_file.conf')
+
+logging.config.fileConfig(fname='/home/khanghoang/project/prescriber_pipeline/src/main/python/util/logging_to_files.conf',disable_existing_loggers=False)
 
 def main():
     try:
+        logging.info("main() is started!!")
         # Get spark objects
         spark = get_spark_object(gav.envn, gav.appName)
         # Validate spark object
         get_curr_date(spark)
 
-
+        logging.info("run_presc_pipeline is compeleted...")
     except Exception as exp:
-        print("Error occured in the main() method. Please check the Stack Trace, " + str(exp))
+        logging.info("Error occured in the main() method. Please check the Stack Trace, " + str(exp))
 
 if __name__ == '__main__':  
+    logging.info("run_presc_pipeline is started!!!")
     main()
