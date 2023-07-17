d60fce6a853052cf1807f321aeb3edc38a6cf105
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2023-07-17 23:02:13 +0700

    validate data by print schema

--- src/main/python/bin/validations.py
+++ src/main/python/bin/validations.py
@@ -24,7 +24,7 @@ def get_curr_date(spark):
 
 def df_count(df, dfName):
     try:
-        logger.info(f"The dataframe validation by count df_count() is started for datafram {dfName}...")
+        logger.info(f"The dataframe validation by count df_count() is started for dataframe {dfName}...")
         df_count = df.count()
         logger.info(f"The data frame count is {df_count}.")
     except Exception as exp:
@@ -35,7 +35,7 @@ def df_count(df, dfName):
 
 def df_top10_rec(df, dfName):
     try:
-        logger.info(f"The dataframe validation by count df_top10_rec() is started for datafram {dfName}...")
+        logger.info(f"The dataframe validation by count df_top10_rec() is started for dataframe {dfName}...")
         logger.info(f"The dataframe top 10 records are: ")
         df_pandas = df.limit(10).toPandas()
         logger.info('\n \t' + df_pandas.to_string(index=False))
@@ -44,3 +44,18 @@ def df_top10_rec(df, dfName):
         raise
     else:
         logger.info(f"The dataframe validation by top 10 records df_top10_rec() is completed.")
+
+
+def df_print_schema(df, dfName):
+    try:
+        logger.info(f"The dataframe schema validation is started for dataframe {dfName}...")
+        schema = df.schema.fields
+        logger.info(f"The data frame {dfName} schema is: ")
+        for i in schema:
+            logger.info(f"\t {i}")
+    except Exception as exp:
+        logger.error("Error in the method - df_print_schema(). Please check the Stack Trace, " + str(exp), exc_info=True)
+        raise
+    else:
+        logger.info(f"The dataframe schema validation is completed.")
+
