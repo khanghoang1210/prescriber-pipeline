8db29942555dc06f852aed9a93407841c997862d
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2023-07-17 17:44:46 +0700

    validate for load files function

--- src/main/python/bin/validations.py
+++ src/main/python/bin/validations.py
@@ -21,3 +21,26 @@ def get_curr_date(spark):
         raise
     else:
         logger.info("Spark object is validated. Spark object is ready.")
+
+def df_count(df, dfName):
+    try:
+        logger.info(f"The dataframe validation by count df_count() is started for datafram {dfName}...")
+        df_count = df.count()
+        logger.info(f"The data frame count is {df_count}.")
+    except Exception as exp:
+        logger.error("Error in the method - df_count(). Please check the Stack Trace, " + str(exp), exc_info=True)
+        raise
+    else:
+        logger.info(f"The dataframe validation by count df_count() is completed.")
+
+def df_top10_rec(df, dfName):
+    try:
+        logger.info(f"The dataframe validation by count df_top10_rec() is started for datafram {dfName}...")
+        logger.info(f"The dataframe top 10 records are: ")
+        df_pandas = df.limit(10).toPandas()
+        logger.info('\n \t' + df_pandas.to_string(index=False))
+    except Exception as exp:
+        logger.error("Error in the method - df_top10_rec(). Please check the Stack Trace, " + str(exp), exc_info=True)
+        raise
+    else:
+        logger.info(f"The dataframe validation by top 10 records df_top10_rec() is completed.")
