eaa0798dbaf901df6e6171c3289069a85d75c083
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2023-07-19 13:19:24 +0700

    changes scripts to submit spark job to yarn

--- src/main/python/bin/get_all_variables.py
+++ src/main/python/bin/get_all_variables.py
@@ -1,7 +1,8 @@
 import os
 import path
 # Set enviroment variables
-os.environ['envn'] = 'TEST'
+#os.environ['envn'] = 'TEST'
+os.environ['envn'] = 'PROD'
 os.environ['header'] = 'True'
 os.environ['inferSchema'] = 'True'
 
@@ -13,7 +14,8 @@ inferSchema = os.environ['inferSchema']
 # Set other variables
 appName = "USA Prescriber Researcher Report"
 current_path = os.getcwd()
-staging_dim_city = current_path + '/src/main/python/staging/dimension_city'
-fact = current_path + '/src/main/python/staging/fact'
-
+# staging_dim_city = current_path + '/src/main/python/staging/dimension_city'
+# fact = current_path + '/src/main/python/staging/fact'
+staging_dim_city = 'PrescPipeline/staging/dimension_city'
+fact = 'PrescPipeline/staging/fact'
 
--- src/main/python/bin/run_presc_pipeline.py
+++ src/main/python/bin/run_presc_pipeline.py
@@ -9,6 +9,7 @@ from data_transform import city_report, top_5_Prescribers
 import logging
 import logging.config
 import os
+from subprocess import PIPE, Popen
 
 # Load the logginf configuration file
 #logging.config.fileConfig(fname='../util/logging_to_file.conf')
@@ -23,35 +24,62 @@ def main():
         # Validate spark object
         get_curr_date(spark)
         # Ingest dimension file
-        for file in os.listdir(gav.staging_dim_city):
-            print("file is " + file)
-            file_dir = gav.staging_dim_city +  '/' + file
-            print(file_dir)
-            if file.split('.')[1] == 'csv':
-                file_format = 'csv'
-                header = gav.header
-                inferSchema = gav.inferSchema
-            elif file.split('.')[1] == 'parquet':
-                file_format = 'parquet'
-                header = 'NA'
-                inferSchema = 'NA'
+        # for file in os.listdir(gav.staging_dim_city):
+        #     print("file is " + file)
+        #     file_dir = gav.staging_dim_city +  '/' + file
+        #     print(file_dir)
+        #     if file.split('.')[1] == 'csv':
+        #         file_format = 'csv'
+        #         header = gav.header
+        #         inferSchema = gav.inferSchema
+        #     elif file.split('.')[1] == 'parquet':
+        #         file_format = 'parquet'
+        #         header = 'NA'
+        #         inferSchema = 'NA'
+
+        # Load the dimension file
+        file_dir = 'PrescPipeline/staging/dimension_city'
+        proc = Popen(["hdfs", "dfs", "-ls", "-C", file_dir], stdout=PIPE, stderr=PIPE)
+        (out,err) = proc.communicate()
+        if 'parquet' in out.decode():
+            file_format = 'parquet'
+            inferSchema = 'NA'
+            header = 'NA'
+        elif 'csv' in out.decode():
+            file_format = 'csv'
+            inferSchema = gav.inferSchema
+            header = gav.header
         # Validate dimesion file
         df_city = load_files(spark=spark, file_format=file_format, file_dir=file_dir, header=header, inferSchema=inferSchema)
         df_count(df_city,'df_city')
         df_top10_rec(df_city, 'df_city')
         # Ingest fact file
-        for file in os.listdir(gav.fact):
-            print("file is " + file)
-            file_dir = gav.fact +  '/' + file
-            print(file_dir)
-        if file.split('.')[1] == 'csv':
-            file_format = 'csv'
-            header = gav.header
-            inferSchema = gav.inferSchema
-        elif file.split('.')[1] == 'parquet':
-            file_format = 'parquet'
-            header = 'NA'
-            inferSchema = 'NA'
+        # for file in os.listdir(gav.fact):
+        #     print("file is " + file)
+        #     file_dir = gav.fact +  '/' + file
+        #     print(file_dir)
+        # if file.split('.')[1] == 'csv':
+        #     file_format = 'csv'
+        #     header = gav.header
+        #     inferSchema = gav.inferSchema
+        # elif file.split('.')[1] == 'parquet':
+        #     file_format = 'parquet'
+        #     header = 'NA'
+        #     inferSchema = 'NA'
+
+        # Load fact file
+        file_dir = 'PrescPipeline/staging/fact'
+        proc = Popen(["hdfs", "dfs", "-ls", "-C", file_dir], stdout=PIPE, stderr=PIPE)
+        (out,err) = proc.communicate()
+        if 'parquet' in out.decode():
+           file_format = 'parquet'
+           header='NA'
+           inferSchema='NA'
+        elif 'csv' in out.decode():
+           file_format = 'csv'
+           header=gav.header
+           inferSchema=gav.inferSchema
+
          # Validate fact file
         df_fact = load_files(spark=spark, file_format=file_format, file_dir=file_dir, header=header, inferSchema=inferSchema)
         df_count(df_fact,'df_fact')
@@ -72,8 +100,8 @@ def main():
         df_presc_final = top_5_Prescribers(df_fact_sel)
         df_top10_rec(df_presc_final, 'df_presc_final')
         df_print_schema(df_presc_final, 'df_presc_final')
-        
-        logging.info("run_presc_pipeline is compeleted.")
+
+        logging.info("run_presc_pipeline is Compeleted.")
     except Exception as exp:
         logging.error("Error occured in the main() method. Please check the Stack Trace, " + str(exp), exc_info=True)
 
